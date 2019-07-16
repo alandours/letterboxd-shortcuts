48a3c8a8f8c49ce5b314c8ca58291cc6fc7422a8
Alan Dours <alandours93@gmail.com>
2019-07-16 20:24:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Letterboxd Shortcuts
+
---
+++ shortcutsFilm.js
@@ -0,0 +1,118 @@
+body.addEventListener('keydown', (e) => {
+
+    const targetElement = e.target.tagName.toLowerCase();
+
+    if(targetElement !== 'input' && targetElement !== 'textarea' && targetElement !== 'select'){
+
+        const key = e.key.toLowerCase();
+        
+        switch(key){
+        
+            case 'a':
+                addToWatchlist();
+                break;
+            case 'l':
+                likeFilm();
+                break;
+            case 'n':
+                logNewFilm();
+                break;
+            case 'r':
+                reviewFilm();
+                break;
+            case 'w':
+                watchFilm();
+                break;
+            case 'enter':
+                submitReview();
+                break;
+            case 'escape':
+                closeModal();
+                break;
+        
+        }
+
+    }
+    
+});
+    
+const addToWatchlist = () => {
+
+    const watchlistButtons = document.querySelectorAll('.ajax-click-action.-watchlist');
+
+    const addButton = watchlistButtons[0].parentNode.classList.contains('hidden') ? watchlistButtons[1] : watchlistButtons[0];
+
+    addButton.click();
+
+}
+
+const logNewFilm = () => {
+
+    const logButton = document.querySelector('#add-new-button');
+
+    if(logButton !== null){
+
+        logButton.click();
+
+    }
+
+}
+
+const likeFilm = () => {
+
+    const reviewWindow = document.querySelector('#modal > #add-film.expanded');
+
+    if(reviewWindow != null){
+
+        reviewWindow.querySelector('#film-like-checkbox').checked = !reviewWindow.querySelector('#film-like-checkbox').checked;
+
+    }else{
+
+        const likeButton = document.querySelector('#userpanel .ajax-click-action.-like');
+
+        likeButton.click();
+
+    }
+
+}
+
+const reviewFilm = () => {
+
+    const reviewButton = document.querySelector('.add-this-film');
+
+    reviewButton.click();
+
+}
+
+const watchFilm = () => {
+
+    const watchButton = document.querySelector('.ajax-click-action.-watch');
+
+    watchButton.click();
+
+}
+
+const closeModal = () => {
+
+    const closeButton = document.querySelector('#cboxClose');
+
+    if(closeButton != null){
+
+        closeButton.click();
+
+    }
+
+}
+
+const submitReview = () => {
+
+    const reviewWindow = document.querySelector('#modal > #add-film.expanded');
+    const confirmWindow = document.querySelector('#confirm-modal');
+
+    if(reviewWindow !== null && confirmWindow === null){
+
+        document.querySelector('#diary-entry-submit-button').click();
+
+    }
+
+}
